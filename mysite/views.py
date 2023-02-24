d8ab23916eb53b7b86b29833b29ceb73e625e1cb
ANUJKUMAR2003 <singrohaanuj@gmail.com>
2023-02-24 10:30:34 +0530

    table updated

--- mysite/views.py
+++ mysite/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-data = {
-    'title' : 'HomePage',\
-    'name' : 'kuch bhi'
-}
+items = {
+       'list': [
+           {'id': 1, 'name': 'Smartphone', 'barcode': '372548362548', 'price': 23000},
+           {'id': 2, 'name': 'Laptop', 'barcode': '882548362548', 'price': 60000},
+           {'id': 3, 'name': 'Keyboard', 'barcode': '672548362548', 'price': 3000},
+           {'id': 4, 'name': 'Mouse', 'barcode': '452548362548', 'price': 2500}
+       ]
+       }
 def homePage(request):
-    return render(request,'index.html',data)
+    return render(request,'index.html',items)
 
 def aboutMe(request):
     return HttpResponse("<b>About Me</b>")
